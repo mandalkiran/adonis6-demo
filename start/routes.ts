46e835f54209063ee380dd7c1117be19782d5a26
Anish Ghimire <anishghimire862@gmail.com>
2024-02-07 11:31:14 +0545

    chore: setup database

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,22 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
 import router from '@adonisjs/core/services/router'
+import User from '#models/user'
 
 router.on('/').render('pages/home')
+
+router.get('/users', async () => {
+  const user = new User()
+  user.fullName = 'Anish Ghimire'
+  user.email = `anish${Date.now()}@cleavr.io`
+  user.password = 'password'
+  await user.save()
+  console.log('User created', user.email)
+
+  const users = await User.all()
+  console.log(
+    'Available users',
+    users.map((u) => u.email)
+  )
+})
